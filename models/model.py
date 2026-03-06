a47cc6e81a94dd0f1768142c4cdc445b412e593e
Penjuru Venkata Jaswanth <penjuruvenkatajaswanth@gmail.com>
2026-03-06 12:36:25 +0530

    Remove FAQGenerator class from model.py
    Removed FAQGenerator class and its methods.

--- models/model.py
+++ models/model.py
@@ -1,9 +1,1 @@
-# Model logic for FAQ generation
 
-class FAQGenerator:
-    def __init__(self):
-        pass
-
-    def generate_faq(self, text):
-        # logic for generating FAQs
-        return []
