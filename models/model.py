a214242ccc56df7673e699aec70ca3734a4aac42
Penjuru Venkata Jaswanth <penjuruvenkatajaswanth@gmail.com>
2026-03-06 12:34:42 +0530

    Add FAQGenerator class for FAQ generation logic

---
+++ models/model.py
@@ -0,0 +1,9 @@
+# Model logic for FAQ generation
+
+class FAQGenerator:
+    def __init__(self):
+        pass
+
+    def generate_faq(self, text):
+        # logic for generating FAQs
+        return []
